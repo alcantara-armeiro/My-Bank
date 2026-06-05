c38b0c62c46a01f2b502828c0399b8fc8443be05
Alcantara Armeiro <alcantara.armeiro@gmail.com>
2026-06-05 13:15:15 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,28 @@
-const CACHE_NAME = 'my-bank-pwa-v1';
+const CACHE_NAME = 'mybank-reserva-pwa-v20260605';
 const APP_SHELL = [
   './',
   './index.html',
-  './arquivo.html',
   './manifest.json',
   './icons/icon-192x192.png',
-  './icons/icon-512x512.png',
-  './icons/maskable-192x192.png',
-  './icons/maskable-512x512.png'
+  './icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL).catch(() => Promise.resolve()))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).catch(() => Promise.resolve())
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
